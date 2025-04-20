42de83bae997c9ce1f7e59e116a4bf6907616878
collin-s46 <158760816+collin-s46@users.noreply.github.com>
2025-04-20 17:33:40 -0400

    Update __init__.py

--- study_smart/__init__.py
+++ study_smart/__init__.py
@@ -2,20 +2,24 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_migrate import Migrate
 import os
 
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
+migrate = Migrate()
 login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__, template_folder='templates', static_folder='static')
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///study_smart.db'  # SQLite database
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
     bcrypt.init_app(app)
+    migrate.init_app(app, db)
     login_manager.init_app(app)
 
     from study_smart.models import User
